67996ac8fb4eeb84c3432e998fd75578be600e62
David Hebbeker <david@hebbeker.info>
2024-04-14 12:31:08 +0200

    Add simple test for default state of debounced keys.

--- test/hmi/test_debounce/test_debounce.cpp
+++ test/hmi/test_debounce/test_debounce.cpp
@@ -1,3 +1,4 @@
+#include <input_device_interface/Keypad.hpp>
 #include <unity.h>
 
 void setUp(void)
@@ -8,8 +9,17 @@ void tearDown(void)
 {
 }
 
+void test_default_inactive()
+{
+    Keypad keypad;
+    TEST_ASSERT_FALSE(keypad.isKeyPressed(KeyId::TASK1));
+}
+
 int main()
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_default_inactive);
+
     return UNITY_END();
 }
